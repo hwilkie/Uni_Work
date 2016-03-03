681a443e269e3a2e9afc23dd72dc58bc45bc168f
hwilkie <hughwilkie@vodafone.co.nz>
2016-03-03 16:09:08 +1300

    Successful install?

--- LabControl/LabControl/Program.cs
+++ LabControl/LabControl/Program.cs
@@ -11,6 +11,7 @@ namespace LabControl
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git");
+            Console.WriteLine("Successfully installed Git");
 
         }
     }
