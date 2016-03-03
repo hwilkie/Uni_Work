f2eef4bdca0fd733c09de693e611da1df66c69c9
hwilkie <hughwilkie@vodafone.co.nz>
2016-03-03 22:03:16 +1300

    Adds lab exercises.

---
+++ LabControl/LabControl/Lab1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabControl
+{
+    class Lab1
+    {
+
+        private void Exercise1()
+        {
+            Console.WriteLine(".NET console application has been set up");
+        }
+
+        private void Exercise2()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        private void Exercise3()
+        {
+            Console.WriteLine("Hello Mum");
+        }
+      
+    }
+}
